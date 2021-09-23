9711d67d10a6d93950b0b7cb5730cd62320a155a
RAHURAM the ACHIEVER <rahuramdars@gmail.vcom>
2021-09-23 15:38:11 +0530

    initial commit

---
+++ output.php
@@ -0,0 +1,14 @@
+<?php
+include("rpath.php");
+include(ROOT_PATH."/app/database/db.php");
+$array=
+[
+'title'=> 'Rahuram',
+'body' => 'rtr',
+'topic_id'=> 17,
+'published'=> 1,
+'image' => ROOT_PATH."/assets/images/image_1.png",
+'users_id' =>1];
+//$post=update('posts',9,$array);
+//dump($post);
+?>
